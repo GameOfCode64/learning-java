b58d8dafc22db24549095e20d91293674d138555
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-06-02 15:10:05 +0530

    solve number 22 problems

---
+++ DisariumNumber.java
@@ -0,0 +1,27 @@
+public class DisariumNumber {
+    public static void main(String[] argu) {
+        int n = 77;
+        int copy = n;
+        int sum = 0;
+
+        int digits = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum += (int) Math.pow(rem, digits);
+            digits--;
+        }
+        if (copy == sum) {
+            System.out.println("This is a Disarium Number");
+        } else {
+            System.out.println("This is NOT a Disarium Number");
+        }
+    }
+}
