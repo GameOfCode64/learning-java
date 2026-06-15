b0a6e52dde93999b8aaf1547edc4eb3b4b6ce388
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-06-15 17:58:57 +0530

    solve pattern

---
+++ Pattern/pattern.java
@@ -0,0 +1,47 @@
+public class pattern {
+    static void pattern1(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int 5){}
+
+    public static void main(String[] args) {
+        int n = 5;
+        pattern4(n);
+    }
+}
