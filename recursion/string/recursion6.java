e1f8f4f63c86aaa430ea1153340c0a67e1f16c81
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-07-02 13:09:35 +0530

    string in recursion

---
+++ recursion/string/recursion6.java
@@ -0,0 +1,38 @@
+package string;
+
+public class recursion6 {
+
+    public static String remove(String s, int count) {
+
+        if (count == s.length()) {
+            return "";
+        }
+
+        String current = "";
+
+        if (s.toLowerCase().charAt(count) != 'a') {
+            current = String.valueOf(s.charAt(count));
+        }
+
+        return current + remove(s, count + 1);
+    }
+
+    static String removeApple(String s, int index) {
+        if (index >= s.length()) {
+            return "";
+        }
+
+        if (s.startsWith("apple", index)) {
+            return removeApple(s, index + 5);
+        }
+
+        return s.charAt(index) + removeApple(s, index + 1);
+
+    }
+
+    public static void main(String[] argu) {
+        System.out.println(remove("baccad", 0));
+        System.out.println(removeApple("bapplecdapple", 0));
+    }
+
+}
