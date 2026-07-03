ca878d2dbb65175a46b07167d98adab784f9fd1d
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-07-03 18:25:45 +0530

    backtracking recursion

--- recursion/string/recursion7.java
+++ recursion/string/recursion7.java
@@ -30,8 +30,24 @@ public class recursion7 {
         return list;
     }
 
+    static ArrayList<String> subSeqArrayN(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subSeqArrayN(p + ch, up.substring(1));
+        ArrayList<String> right = subSeqArrayN(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+    }
+
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        System.out.println(subSeqArray("", "abc", list));
+        // ArrayList<String> list = new ArrayList<>();
+        System.out.println(subSeqArrayN("", "abc"));
     }
 }
