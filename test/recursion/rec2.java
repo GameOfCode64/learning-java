6f8276f672fc4a5a3cf9af2ee1fab2961e6bb594
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-07-05 11:55:49 +0530

    learn Subsequences using recurision

---
+++ test/recursion/rec2.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class rec2 {
+    static void subSeq(String p, String up) {
+
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subSeq(p + ch, up.substring(1));
+        subSeq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subSeqRet(String p, String up) {
+
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        int ascii = (int) ch;
+        ArrayList<String> left = subSeqRet(p + ch, up.substring(1));
+        ArrayList<String> right = subSeqRet(p, up.substring(1));
+        ArrayList<String> asciiBranch = subSeqRet(p + ascii, up.substring(1));
+        left.addAll(right);
+        left.addAll(asciiBranch);
+        return left;
+    }
+
+    public static void main(String[] argu) {
+        System.out.println(subSeqRet("", "abc"));
+    }
+}
