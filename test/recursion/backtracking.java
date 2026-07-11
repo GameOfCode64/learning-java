ed629721c531096f7f9c7a26179231673dff912a
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-07-11 17:42:38 +0530

    solve to mid problem in backtracking

--- test/recursion/backtracking.java
+++ test/recursion/backtracking.java
@@ -1,35 +1,43 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class backtracking {
 
-    static void subsets(int[] arr, int index, ArrayList<Integer> current) {
-        if (index >= arr.length) {
-            System.out.println(current);
-            return;
-        }
-        current.add(arr[index]);
-        subsets(arr, index + 1, current);
+    public static List<List<Integer>> subsets(int[] nums) {
 
-        current.remove(current.size() - 1);
-        subsets(arr, index + 1, current);
+        List<List<Integer>> result = subsetsList(nums, 0, new ArrayList<>());
+
+        return result;
+    }
+
+    static void subsets2(int[] arr, int index, ArrayList<Integer> current) {
+    if (index >= arr.length) {
+    System.out.println(current);
+    return;
+    }
+    current.add(arr[index]);
+    subsets(arr, index + 1, current);
+
+    current.remove(current.size() - 1);
+    subsets(arr, index + 1, current);
 
     }
 
-    static ArrayList<ArrayList<Integer>> subsetsList(int[] arr, int index, ArrayList<Integer> current) {
+    static List<List<Integer>> subsetsList(int[] arr, int index, ArrayList<Integer> current) {
 
         if (index >= arr.length) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            List<List<Integer>> list = new ArrayList<>();
             list.add(new ArrayList<>(current));
             return list;
         }
 
         current.add(arr[index]);
-        ArrayList<ArrayList<Integer>> left = subsetsList(arr, index + 1, current);
+        List<List<Integer>> left = subsetsList(arr, index + 1, current);
 
         current.remove(current.size() - 1);
-        ArrayList<ArrayList<Integer>> right = subsetsList(arr, index + 1, current);
+        List<List<Integer>> right = subsetsList(arr, index + 1, current);
 
         left.addAll(right);
         return left;
@@ -70,10 +78,29 @@ public class backtracking {
 
     }
 
+    static void subsetsOfSumLessTarget(int[] arr, int index, int sum, int target, ArrayList<Integer> current) {
+
+        if (index >= arr.length) {
+            if (sum < target) {
+                System.out.println(current);
+            }
+            return;
+        }
+        if (sum >= target) {
+            return;
+        }
+
+        current.add(arr[index]);
+        subsetsOfSumLessTarget(arr, index + 1, sum + arr[index], target, current);
+
+        current.remove(current.size() - 1);
+        subsetsOfSumLessTarget(arr, index + 1, sum, target, current);
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = { 1, 2, 3, 4 };
+        int[] arr = { 1, 3, 4 };
 
-        subsetsSumFive(arr, 0, 0, 5, new ArrayList<>());
+        subsetsOfSumLessTarget(arr, 0, 0, 6, new ArrayList<>());
     }
 }
