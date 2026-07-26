f942f5866f29bc38e081c68b6db5fb4a0f1013a7
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-07-26 15:41:53 +0530

    fix imports

--- maths/MaximumProduct.java
+++ maths/MaximumProduct.java
@@ -1,9 +1,11 @@
-public class MaximumProduct{
-   public Static  int maximumProduct(int[] nums) {
+import java.util.Arrays;
 
-       Arrays.sort(nums);
-       int n = nums.length;
-        
-       return Math.max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
+public class MaximumProduct {
+    public static int maximumProduct(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        return Math.max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
     }
 }
