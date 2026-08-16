77576f9803255595c50469143e18c4c670677e49
Bhavishya Tripathi <gameofcode972@gmail.com>
2026-08-16 12:02:22 +0530

    Learn about enums and oops complate!

---
+++ oops/Class7.java
@@ -0,0 +1,12 @@
+public class Class7 {
+    enum DAYS {
+         MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+    
+    public static void main(String[] args){
+        DAYS today = DAYS.MONDAY;
+
+        System.out.println("Today is: " + today);
+
+    }
+}
